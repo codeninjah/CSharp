5f3b82e622d66d8a449e13afe66b54a2ec0dfeaa
Alexandry Cornita <alexandry.cornita.spo16@ecusolna.onmicrosoft.com>
2017-09-18 09:31:06 +0200

    Skapat olika classer för Varukorgen

---
+++ VarukorgTestsLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VarukorgTestsLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VarukorgTestsLib")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b675d60-3cda-4b79-bc61-29a5687138ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VarukorgTestsLib/VarukorgTestsLib.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VarukorgTestsLib
+{
+	[TestClass]
+	public class VarukorgTestsLib
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Kund testKund = new Kund("Test Testsson");
+			Produkt testLaptop = new Produkt("Laptop test");
+			testLaptop.Pris = 4000;
+			Produkt testSkärm = new Produkt("Skärm test");
+			testSkärm.Pris = 1500;
+			Varukorg sut = new Varukorg();
+			sut.testKund;
+			sut.LäggTillProdukt(testLaptop);
+			sut.LäggTillProdukt(testSkärm);
+
+			var expected = "Test Testsson har beställt varor för 5500 kr";
+
+			// Act
+			sut.Beställ();
+			Orderbekräftelse result = sut.Orderbekräftelse;
+			string actual = result.Meddelande;
+
+			// Assert
+			Assert.AreEqual(expected, actual);
+
+
+		}
+	}
+}
